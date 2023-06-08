9d13aee594d1b77c130a513625994a4f2e80f06b
Mirzarahmon <rahmonraxmatov@gmail.com>
2023-06-09 02:13:12 +0500

    added css

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let anchors = document.querySelectorAll('a[href^="#"]');
-
+let button = document.querySelector(".scroll");
 for (let anchor of anchors) {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
@@ -12,3 +12,17 @@ for (let anchor of anchors) {
     });
   });
 }
+window.addEventListener("scroll", (el) => {
+  let scrol = this.scrollY;
+  if (this.scrollY >= 100) {
+    button.classList.add("add");
+  } else if (this.scrollY < 500) {
+    button.classList.remove("add");
+  }
+});
+window.addEventListener("scroll", () => {
+  header.classList.toggle("sticky", window.scrollY >= 0);
+});
+button.addEventListener("click", () => {
+  scrollTo(0, 0);
+});
